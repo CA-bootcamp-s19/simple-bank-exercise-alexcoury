b60e2ccd5736aefa5305debd1727a164f7eea91a
Alex Coury <alexcoury@MacBook-Pro.localdomain>
2019-06-14 23:23:11 -0400

    fixed a bug

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -31,11 +31,11 @@ contract SimpleBank {
     event LogEnrolled(address accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade(address accountAddress, uint depositAmount);
+    event LogDepositMade(address accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint indexed newBalance);
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
     //
     // Functions
